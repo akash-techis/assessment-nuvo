7054cca363ff4543e5743009d0fbd154d9f1324f
shekh1234 <akash@techis.io>
2022-02-26 05:39:37 +0530

    rating

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -15,7 +15,7 @@ const Description = ({ product, selected }) => {
         <h2>${product.price}</h2>
         <h4>{product.description}</h4>
         <h3>rating: {product.rating.rate}</h3>
-        <h3>count: {product.rating.count}</h3>
+        {/* <h3>count: {product.rating.count}</h3> */}
         <button>Add to Cart</button>
       </div>
     </div>
